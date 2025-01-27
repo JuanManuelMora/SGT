7bec9806a5f3e7aabf4974fda5c3aa2fdce371a8
JuanManuelMora <juanmanuelmorales668@gmail.com>
2025-01-27 11:16:18 -0500

    Creación e implementación del CRUD.

--- tareas/urls.py
+++ tareas/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_tareas, name = 'lista_tareas'),
+    path('crear/', views.crear_tarea, name = 'crear_tarea'),
+    path('editar/<int:id>/', views.editar_tarea, name = 'editar_tarea'),
+    path('eliminar/<int:id>/', views.eliminar_tarea, name = 'eliminar_tarea'),
+    path('completar/<int:id>/', views.completar_tarea, name = 'completar_tarea'),
 ]
--- tareas/views.py
+++ tareas/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tarea
 
 # Create your views here.
 def lista_tareas(request):
     tareas = Tarea.objects.all()
     return render(request, 'tareas/lista_tareas.html', {'tareas': tareas} )
+
+def crear_tarea(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        descripcion = request.POST.get('descripcion')
+        fecha_vencimiento = request.POST.get('fecha_vencimiento')
+        Tarea.objects.create(
+            titulo = titulo,
+            descripcion = descripcion,
+            fecha_vencimiento = fecha_vencimiento
+        )
+        return redirect('lista_tareas')
+    return render(request, 'tareas/crear_tarea.html')
+
+def editar_tarea(request, id):
+    tarea = get_object_or_404(Tarea, id = id)
+    if request.method == 'POST':
+        tarea.titulo = request.POST.get('titulo')
+        tarea.descripcion = request.POST.get('descripcion')
+        tarea.fecha_vencimiento = request.POST.get('fecha_vencimiento')
+        tarea.save()
+        return redirect('lista_tareas')
+    return render(request, 'tareas/editar_tarea.html', {'tarea' : tarea})
+
+def eliminar_tarea(request, id):
+    tarea = get_object_or_404(Tarea, id = id)
+    if request.method == 'POST':
+        tarea.delete()
+        return redirect('lista_tareas')
+    return render(request, 'tareas/eliminar_tarea.html', {'tarea' : tarea})
+
+def completar_tarea(request, id):
+    tarea = get_object_or_404(Tarea, id = id)
+    tarea.completada = True
+    tarea.save()
+    return redirect('lista_tareas')
