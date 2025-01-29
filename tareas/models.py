b30dc470ab1f64268d0be03dcfb92035623da196
JuanManuelMora <juanmanuelmorales668@gmail.com>
2025-01-29 11:53:56 -0500

    Mejorar el diseño de las plantillas para que sean más atractivas y responsive.
    También hubo cambios en el nombre de la tabla de base de datos y sus columnas.

--- tareas/models.py
+++ tareas/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+# importaciones para las validaciones de no crear tareas sin fecha de vencimiento ni tampoco permitir fechas de
+# vencimiento posteriores a la fecha actual.
+from django.core.exceptions import ValidationError
+from django.utils.timezone import now
+
+def validar_fecha_futura(value):
+    if value < now():
+        raise ValidationError('La fecha de vencimiento no puede ser anterior a la fecha actual.')
 
 # Create your models here.
 class Tarea(models.Model):
-    titulo = models.CharField(max_length = 200)
-    descripcion = models.TextField()
-    completada = models.BooleanField(default = False)
-    fecha_creacion = models.DateTimeField(auto_now_add = True)
-    fecha_vencimiento = models.DateTimeField(null = True, blank = True)
+    titulo = models.CharField(max_length = 200, db_column='sgt_titulo')
+    descripcion = models.TextField(db_column='sgt_descripcion')
+    completada = models.BooleanField(default = False, db_column='sgt_completada')
+    fecha_creacion = models.DateTimeField(auto_now_add = True, db_column='sgt_fechaCreacion')
+    fecha_vencimiento = models.DateTimeField(null = True, blank = True, db_column='sgt_fechaVencimiento')
+
+    class Meta:
+        db_table = 'sgt'  # Usar minúsculas para evitar problemas con PostgreSQL
 
     def __str__(self):
         return self.titulo
--- tareas/views.py
+++ tareas/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils.timezone import now
 from .models import Tarea
+from datetime import datetime
+from django.utils.timezone import make_aware 
 
 # Create your views here.
 def lista_tareas(request):
@@ -11,6 +14,18 @@ def crear_tarea(request):
         titulo = request.POST.get('titulo')
         descripcion = request.POST.get('descripcion')
         fecha_vencimiento = request.POST.get('fecha_vencimiento')
+
+        # Validación: La fecha de vencimiento no puede ser nula ni menor a la fecha actual
+        if not fecha_vencimiento:
+            return render(request, 'tareas/crear_tarea.html', {'error': 'Debes seleccionar una fecha de vencimiento'})
+        
+        fecha_vencimiento = datetime.strptime(fecha_vencimiento, "%Y-%m-%dT%H:%M")
+
+        fecha_vencimiento = make_aware(fecha_vencimiento)
+
+        if fecha_vencimiento < now():
+            return render(request, 'tareas/crear_tarea.html', {'error': 'La fecha de vencimiento no puede ser anterior a la fecha actual.'})
+
         Tarea.objects.create(
             titulo = titulo,
             descripcion = descripcion,
